b301764df4da3a2a316d498360f55feca204d32d
Bruno Barbagelata <bruno140685@gmail.com>
2021-11-28 19:29:44 -0500

    backend server.js

--- backend/server.js
+++ backend/server.js
@@ -12,6 +12,8 @@ app.get("/api/products", (req, res) => {
   res.json(data.products);
 });
 
-app.listen(5000, () => {
-  console.log("Serve at http://localhost:5000");
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => {
+  console.log(`Serve at http://localhost:${port}`);
 });
