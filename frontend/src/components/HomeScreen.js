a65cc4ee68dc08d979deb461b796c9bbf8a2d128
Bruno Barbagelata <bruno140685@gmail.com>
2021-12-02 00:03:45 -0500

    added Redux

--- frontend/src/components/HomeScreen.js
+++ frontend/src/components/HomeScreen.js
@@ -1,27 +1,17 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect } from "react";
 import Product from "./Product";
 import LoadingBox from "./LoadingBox";
 import MessageBox from "./MessageBox";
-import axios from "axios";
+import { useDispatch, useSelector } from "react-redux";
+import { listProducts } from "../actions/productActions";
 
 function HomeScreen(props) {
-  const [products, setProducts] = useState([]);
-  const [loading, setLoading] = useState(false);
-  const [error, setError] = useState(false);
+  const dispatch = useDispatch();
+  const productList = useSelector((state) => state.productList);
+  const { loading, error, products } = productList;
 
   useEffect(() => {
-    const fetchData = async () => {
-      try {
-        setLoading(true);
-        const { data } = await axios.get("http://localhost:5000/api/products");
-        setLoading(false);
-        setProducts(data);
-      } catch (err) {
-        setError(err.message);
-        setLoading(false);
-      }
-    };
-    fetchData();
+    dispatch(listProducts());
   }, []);
 
   return (
